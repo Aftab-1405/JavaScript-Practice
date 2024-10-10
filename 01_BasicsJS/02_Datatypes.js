7682475e1a6454bda26822a53c23c231c23a1410
AFTAB NADAF <128632799+Aftab-1405@users.noreply.github.com>
2024-10-10 07:42:00 +0000

    In this section I got an exposure of various datatypes supported in js and what is the difference between them.

---
+++ 01_BasicsJS/02_Datatypes.js
@@ -0,0 +1,11 @@
+/**
+ * Datatypes in JavaScript:
+ * 1. Number => It has range 2^53-1
+ * 2. String => "Aftab"
+ * 3. Boolean => "true/false"
+ * 4. Undefined => This is the type of value "undefined", this happens when variable in declared but not yet initiated. 
+ e.g., let name; 
+ * 5. Object
+ * 6. bigInt
+ * 7. null => Standalone value which can be assigned to variables.
+ */
