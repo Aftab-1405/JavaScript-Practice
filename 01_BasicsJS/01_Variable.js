4ae6ddc050289bdc9c6899c47a8a49ad6fe85830
AFTAB NADAF <128632799+Aftab-1405@users.noreply.github.com>
2024-10-10 07:21:02 +0000

    Installed prettier extension for JS code formatting.

--- 01_BasicsJS/01_Variable.js
+++ 01_BasicsJS/01_Variable.js
@@ -1,5 +1,5 @@
 const accountID = 100; // This sysntax is preferable to declare and initialize constants in javascript.
-let userName = "Aftab1405" // This is used to declare and initialize varibales who can change their valued during the execution of program.
+let userName = "Aftab1405"; // This is used to declare and initialize varibales who can change their valued during the execution of program.
 var passowrdUser = "Abn@1405"; // This is the older way of defining variable in js, but not recommended becuase it has scope problem in code.
 cityOfLiving = "Hyderabad"; // This is another way, same like we do in python but not recommended.
 let currentAddress; // If any variable is not initialized in js then its default value is "undefined".
@@ -9,4 +9,10 @@ let currentAddress; // If any variable is not initialized in js then its default
 */
 
 console.log(accountID); // Used to print single value.
-console.table([accountID, userName, passowrdUser, cityOfLiving, currentAddress]);
+console.table([
+  accountID,
+  userName,
+  passowrdUser,
+  cityOfLiving,
+  currentAddress,
+]);
