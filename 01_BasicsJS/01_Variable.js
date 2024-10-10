2da3cbec5e55b45379d9006009f6900e2a221177
AFTAB NADAF <128632799+Aftab-1405@users.noreply.github.com>
2024-10-10 07:10:35 +0000

    Studied of the ways of creating variables in js, and also had hands on practice on it in "01_Variables.js" file.

---
+++ 01_BasicsJS/01_Variable.js
@@ -0,0 +1,12 @@
+const accountID = 100; // This sysntax is preferable to declare and initialize constants in javascript.
+let userName = "Aftab1405" // This is used to declare and initialize varibales who can change their valued during the execution of program.
+var passowrdUser = "Abn@1405"; // This is the older way of defining variable in js, but not recommended becuase it has scope problem in code.
+cityOfLiving = "Hyderabad"; // This is another way, same like we do in python but not recommended.
+let currentAddress; // If any variable is not initialized in js then its default value is "undefined".
+
+/*
+    Note : Now-a-days only two way of variable creation is recommended in js - let and const.
+*/
+
+console.log(accountID); // Used to print single value.
+console.table([accountID, userName, passowrdUser, cityOfLiving, currentAddress]);
