866cc172d9ef7031f8ce09689599971a39ea305b
AFTAB NADAF <128632799+Aftab-1405@users.noreply.github.com>
2024-10-10 11:20:12 +0000

    Starting javascript string topic. Farther I have to learn all the string methods of it.

---
+++ 01_BasicsJS/04_StringsInJS.js
@@ -0,0 +1,6 @@
+let firstName = "Aftab";
+let lastName = "Nadaf";
+
+console.log(`Guys my first name is ${firstName} and my last name is ${lastName}.`);
+
+// I have to learn all the string methods available in js and practive them all.
