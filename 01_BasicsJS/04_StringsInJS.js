61b3da4526388a1c0dd89eb99f2ac7f9cbb861fc
AFTAB NADAF <128632799+Aftab-1405@users.noreply.github.com>
2024-10-11 04:45:47 +0000

    Tried some most common methods of string and practiced them.

--- 01_BasicsJS/04_StringsInJS.js
+++ 01_BasicsJS/04_StringsInJS.js
@@ -1,6 +1,12 @@
 let firstName = "Aftab";
+let middleName = "  Bebijan maqbool nadaf"
 let lastName = "Nadaf";
 
 console.log(`Guys my first name is ${firstName} and my last name is ${lastName}.`);
 
 // I have to learn all the string methods available in js and practive them all.
+console.log(middleName.toUpperCase().trim());
+console.log(middleName.charAt(3));
+console.log(middleName.trim());
+console.log(middleName.split(" "));
+
