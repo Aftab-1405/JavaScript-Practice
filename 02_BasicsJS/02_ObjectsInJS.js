8576cb3e45120a3769203461d9beeb30d5d84638
AFTAB NADAF <128632799+Aftab-1405@users.noreply.github.com>
2024-10-13 09:26:24 +0000

    Learned the two main ways to creat an objects in JS. Also learned the spread operator used to merge the two or more objects into single one.

--- 02_BasicsJS/02_ObjectsInJS.js
+++ 02_BasicsJS/02_ObjectsInJS.js
@@ -1,12 +1,20 @@
+// This is the way where we can create a singleton object using constructor.
+let myObject = new Object();
+myObject.id = "Aftab";
+myObject.name = "Nadaf";
+
+console.log(myObject);
+
 let pincode = Symbol(); // Create a unique symbol for pincode
 
+// This is the literal way to create an object in javascript.
 let employeeDetails = {
     name: "Aftab Nadaf",
     companyName: "Quality Kiosk",
     contact: 7757875327,
     payPackage: 350000,
     [pincode]: 416418 // Use the symbol as a key
-}
+};
 
 console.log(employeeDetails["name"]);     // Aftab Nadaf
 console.log(employeeDetails["pincode"]);  // undefined (string key doesn't exist)
@@ -26,3 +34,23 @@ delete employeeDetails.contact; // This will not delete the property
 
 console.log(employeeDetails);
 // Output: { name: "Aftab Nadaf", companyName: "Quality Kiosk", contact: 7757875327, payPackage: 350000 }
+
+// Merging two objects using the spread operator
+let object1 = {
+    id: "Aftab",
+    role: "Software Engineer"
+};
+
+let object2 = {
+    companyName: "Quality Kiosk",
+    contact: 7757875327
+};
+
+// Merging object1 and object2 using spread operator
+let mergedObject = {
+    ...object1,
+    ...object2
+};
+
+console.log(mergedObject);
+// Output: { id: "Aftab", role: "Software Engineer", companyName: "Quality Kiosk", contact: 7757875327 }
