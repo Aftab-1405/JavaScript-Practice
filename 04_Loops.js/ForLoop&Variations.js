be29e8155c9a861e9cd8432f26cc4d2e683bc4a8
AFTAB NADAF <128632799+Aftab-1405@users.noreply.github.com>
2024-10-19 06:51:19 +0000

    Again gone to the ForLoop&Variations.js file and learned how to iterate over an array of object using for-of loop.

--- 04_Loops.js/ForLoop&Variations.js
+++ 04_Loops.js/ForLoop&Variations.js
@@ -27,3 +27,37 @@ let bolyActress = {
 for (const key in bolyActress) {
     console.log(bolyActress[key]);
 }
+
+
+
+const libraryBooks = [
+    {
+        title: "The Great Gatsby",
+        author: "F. Scott Fitzgerald",
+        isbn: "9780743273565",
+        available: true
+    },
+    {
+        title: "1984",
+        author: "George Orwell",
+        isbn: "9780451524935",
+        available: false
+    },
+    {
+        title: "To Kill a Mockingbird",
+        author: "Harper Lee",
+        isbn: "9780061120084",
+        available: true
+    },
+    {
+        title: "Pride and Prejudice",
+        author: "Jane Austen",
+        isbn: "9781503290563",
+        available: true
+    }
+];
+
+
+for (const element of libraryBooks) {
+    console.log(element["title"]);      
+}
